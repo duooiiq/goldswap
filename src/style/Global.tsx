c45cceb80eff7cc662c005d068fd393fbab10ef4
The MexToken Proyect <83620760+duooiiq@users.noreply.github.com>
2021-08-26 18:35:41 -0500

    Create Global.tsx

--- src/style/Global.tsx
+++ src/style/Global.tsx
@@ -9,10 +9,144 @@ declare module 'styled-components' {
 
 const GlobalStyle = createGlobalStyle`
   * {
-    font-family: 'Kanit', sans-serif;
+    font-family: Monospace;
   }
   body {
-    background-color: ${({ theme }) => theme.colors.background};
+    background-image: linear-gradient(35deg, purple, gold, blue, gold, purple);
+  div.ggaBjV {
+    background-image: linear-gradient(35deg, purple, gold, blue, gold, purple);
+    background: linear-gradient(35deg, purple, gold, blue, gold, purple);
+  }
+  .fobcVZ {
+ background: transparent;
+  }
+  .eUmtTA {
+    background-color: #ffffff;
+  }
+  .imFMeb {
+  background-color: transparent;
+  }
+  .LUmcf {
+    color: #FFFFFF;
+  }
+  .kKnXie {
+    color: #000000;
+  }
+  .ksnaJT {
+    background-color: #b70c1f;
+    color: #b3b3b3;
+  }
+  .hIkwok {
+    background-color: transparent;
+  }
+  .gbIzQI {
+    background-color: linear-gradient(55deg, red, blue);
+    color: red;
+  }
+  a.sc-GvhzO bssNqU {
+    background-image: url("https://i.ibb.co/t3V0hJL/1629736129850.png");
+  }
+  .jskajajajajajaajajafre {
+    background-image: url("https://i.ibb.co/t3V0hJL/1629736129850.png");
+  }
+  .cLWmML {
+    color: red;
+  }
+  .dlqLfN {
+    background-color: red;
+  }
+  .WFckb {
+    fill: red;
+  }
+  .iokwTE {
+        background-color: #eeeaf400;
+            border-radius: 16px;
+                display: -webkit-inline-box;
+                    display: -webkit-inline-flex;
+                        display: -ms-inline-flexbox;
+                            display: inline-flex;
+                                border: 1px solid #d7caec00;
+                                }
+  }
+  .eksRHR {
+  border-color: transparent;
+  color: red;
+  }
+  .JKEDj {
+    background-color: transparent;
+  }
+  .MdleP, .cgMtet, .iwBuoH, .jPcoil {
+    color: #FFFFFF;
+  }
+  .jfiVtQ {
+    color: black;
+  }
+  sc-iiBnNu faPyQS token-amount-input::placeholder {
+color: black;
+  }
+  .dvUMTZ {
+    color: red;
+  }
+  .dwlfZz {
+    background-color: red;
+  }
+  .lltITY, .kFooQR {
+    color: red;
+    border-color: transparent;
+    background-color: transparent;
+  }
+  .ePVsVU, .bJFCMa {
+    box-shadow: 0px 0px 0px 1px red, 0px 0px 0px 4px rgb(49 208 170 / 20%);
+    background-color: transparent;
+  }
+  .bJFCMa:focus:not(:disabled) {
+    box-shadow: 0px 0px 0px 1px red, 0px 0px 0px 4px rgb(49 208 170 / 20%);
+  }
+  .ePVsVU:focus:not(:disabled) {
+    box-shadow: 0px 0px 0px 1px red, 0px 0px 0px 4px rgb(49 208 170 / 20%);
+  }
+  .hWRKzT, .dPIQsB, .hWRKzT, .leAbBL {
+    color: white;
+  }
+  .cSiPfC {
+    background-color: transparent;
+  }
+  .bssNqU .desktop-icon {
+    display: none;
+  }
+  .hIkwok {
+    background-image: url('https://i.ibb.co/jrk24fm/GOLDSWAP.png');
+    background-repeat: no-repeat;
+    background-position: top center;
+    background-size: 140px 55px;
+    height: 48px;
+  }
+  .dcRZAS {
+    background: red;
+  }
+  div.pancakes {
+    display: none;
+    background: transparent;
+  }
+  .jGjMfK, .fjFJYx {
+    background-color: gold;
+  .dGkvIx, .hUyfFq {
+    background-color: gold;
+    color: black;
+  }
+  .kfJDrs {
+    display: none;
+  }
+  .hMIuvo svg, .LtDiV {
+    fill: #ba0f22;
+  }
+  .fngDZl {
+    fill: white;
+  }
+  .bssNqU {
+    display: none;
+  }
+
 
     img {
       height: auto;
