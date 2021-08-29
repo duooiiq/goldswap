9dc1a95479da74c548512b439ca64140cdf6bdc6
The MexToken Proyect <83620760+duooiiq@users.noreply.github.com>
2021-08-28 19:53:35 -0500

    Update config.ts

--- src/components/Menu/config.ts
+++ src/components/Menu/config.ts
@@ -3,12 +3,12 @@ import { ContextApi } from 'contexts/Localization/types'
 
 const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
   {
-    label: t('Home'),
+    label: t('HomePage'),
     icon: 'HomeIcon',
     href: '/',
   },
   {
-    label: t('Trade'),
+    label: t('DEX And Liquidity'),
     icon: 'TradeIcon',
     items: [
       {
@@ -19,10 +19,6 @@ const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
         label: t('Liquidity'),
         href: '/pool',
       },
-      {
-        label: t('LP Migration'),
-        href: 'https://v1exchange.pancakeswap.finance/#/migrate',
-      },
     ],
   },
   {
@@ -31,46 +27,9 @@ const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
     href: '/farms',
   },
   {
-    label: t('Pools'),
-    icon: 'PoolIcon',
-    href: '/pools',
-  },
-  {
-    label: t('Prediction (BETA)'),
-    icon: 'PredictionsIcon',
-    href: '/prediction',
-  },
-  {
-    label: t('Lottery'),
-    icon: 'TicketIcon',
-    href: '/lottery',
-    status: {
-      text: t('Win').toLocaleUpperCase(),
-      color: 'success',
-    },
-  },
-  {
-    label: t('Collectibles'),
-    icon: 'NftIcon',
-    href: '/collectibles',
-  },
-  {
-    label: t('Team Battle'),
-    icon: 'TeamBattleIcon',
-    href: '/competition',
-  },
-  {
-    label: t('Teams & Profile'),
+    label: t('Profile'),
     icon: 'GroupsIcon',
     items: [
-      {
-        label: t('Leaderboard'),
-        href: '/teams',
-      },
-      {
-        label: t('Task Center'),
-        href: '/profile/tasks',
-      },
       {
         label: t('Your Profile'),
         href: '/profile',
@@ -78,42 +37,16 @@ const config: (t: ContextApi['t']) => MenuEntry[] = (t) => [
     ],
   },
   {
-    label: t('Info'),
-    icon: 'InfoIcon',
-    href: 'https://pancakeswap.info',
-  },
-  {
-    label: t('IFO'),
-    icon: 'IfoIcon',
-    href: '/ifo',
-  },
-  {
     label: t('More'),
     icon: 'MoreIcon',
     items: [
       {
         label: t('Contact'),
-        href: 'https://docs.pancakeswap.finance/contact-us',
-      },
-      {
-        label: t('Voting'),
-        href: '/voting',
+        href: 'mailto: paperguitar1789@gmail.com',
       },
       {
         label: t('Github'),
-        href: 'https://github.com/pancakeswap',
-      },
-      {
-        label: t('Docs'),
-        href: 'https://docs.pancakeswap.finance',
-      },
-      {
-        label: t('Blog'),
-        href: 'https://pancakeswap.medium.com',
-      },
-      {
-        label: t('Merch'),
-        href: 'https://pancakeswap.creator-spring.com/',
+        href: 'https://github.com/duooiiq',
       },
     ],
   },
