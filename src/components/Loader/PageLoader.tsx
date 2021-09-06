a2e38edfa28e301e62b41ebfcb173392a515478f
The MexToken Proyect <83620760+duooiiq@users.noreply.github.com>
2021-09-06 14:37:29 -0500

    Update PageLoader.tsx

--- src/components/Loader/PageLoader.tsx
+++ src/components/Loader/PageLoader.tsx
@@ -1,9 +1,8 @@
 import React from 'react'
 import styled from 'styled-components'
 import { Spinner } from '@pancakeswap/uikit'
-import Page from '../Layout/Page'
 
-const Wrapper = styled(Page)`
+const Wrapper = styled(styled)`
   display: flex;
   justify-content: center;
   align-items: center;
