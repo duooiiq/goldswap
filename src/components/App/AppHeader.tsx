f5e7c8e3dcf0710616a966060e544cabe5284250
The MexToken Proyect <83620760+duooiiq@users.noreply.github.com>
2021-09-13 10:06:08 -0500

    Update AppHeader.tsx

--- src/components/App/AppHeader.tsx
+++ src/components/App/AppHeader.tsx
@@ -27,9 +27,8 @@ const AppHeader: React.FC<Props> = ({ title, subtitle, helper, backTo, noConfig 
     <AppHeaderContainer>
       <Flex alignItems="center" mr={noConfig ? 0 : '16px'}>
         {backTo && (
-          <IconButton as={Link} to={backTo}>
-            <ArrowBackIcon width="32px" />
-          </IconButton>
+          <div className="tocenter">
+          </div>
         )}
         <Flex flexDirection="column">
           <Heading as="h2" mb="8px">
